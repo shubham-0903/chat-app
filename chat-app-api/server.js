7cbe973dd02f9c8e9ee81e709f2e2768ec58ef56
shubham <shubham@shubhams-MacBook-Pro.local>
2025-11-01 14:32:36 +0530

    initial commit

---
+++ chat-app-api/server.js
@@ -0,0 +1,31 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./src/config/db");
+const logger = require("./src/config/logger");
+const routes = require("./src/routes");
+const morgan = require("morgan");
+
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+app.use(
+  morgan("combined", {
+    stream: {
+      write: (message) => logger.info(message.trim()),
+    },
+  })
+);
+
+// Routes
+app.use("/api", routes);
+
+// Start server
+app.listen(PORT, async () => {
+  await connectDB();
+  logger.info(`API Server running on http://localhost:${PORT}`);
+});
---
+++ chat-app-frontend/src/App.jsx
@@ -0,0 +1,8 @@
+
+export default function App() {
+  return (
+    <h1 className="text-justify font-bold text-fuchsia-900">
+      Hello world!
+    </h1>
+  )
+}
