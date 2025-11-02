a05d00f862dd22fd808aab4b913003b9fdc80f6b
shubham <shubham@shubhams-MacBook-Pro.local>
2025-11-02 16:03:50 +0530

    basic auth flow

---
+++ chat-app-frontend/src/api/axiosInstance.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+
+const axiosInstance = axios.create({
+  baseURL: "http://localhost:4000/api",
+});
+
+// Attach token to headers
+axiosInstance.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) config.headers.Authorization = `Bearer ${token}`;
+  return config;
+});
+
+// Intercept 401 responses
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      localStorage.removeItem("token");
+      window.location.href = "/login";
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default axiosInstance;
